3476ae60e218b18b0322d74e315f2c34ebfe8940
gz-code <gzstores01@gmail.com>
2023-06-18 03:21:10 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0 is Success
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'a'
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+			}
+	putchar('\n');
+	return (0);
+}
